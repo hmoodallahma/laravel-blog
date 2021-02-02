514e5b584765422dfa37f24006976dec6adf026b
Aisha <hmoodallahma@gmail.com>
2021-02-02 18:28:24 -0500

    set up vuex and config for requesting data to update state

---
+++ resources/js/index.js
@@ -0,0 +1,55 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+import { setAuthHeadersFromAuthRequest } from './helpers'
+import * as config from '../config'
+
+// import { setAuthHeadersFromAuthRequest, setAuthHeadersFromStorage } from './helpers'
+
+Vue.use(Vuex)
+
+// axios.defaults.xsrfCookieName = 'csrftoken'
+// axios.defaults.xsrfHeaderName = 'X-CSRFToken'
+// acrossDomain: true
+
+export const store = new Vuex.Store({
+  data () {
+    return {
+      msg: {},
+      windowWidth: null,
+      posts: []
+    }
+  },
+  state: {
+    
+    endpoints: {
+      // TODO: Remove hardcoding of dev endpoints
+      baseUrl: config.apiDomain,
+      getPosts: config.apiPosts,
+    }
+  },
+  mutations: {
+    setPosts (state, posts) {
+      state.posts = posts
+    }
+  },
+  actions : {
+  obtainPosts (context) {
+      axios.post(`${this.state.endpoints.getPosts}`)
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error.response)
+          context.errors = error.response
+        })
+    },
+  }
+  beforeDestroy () {
+    window.removeEventListener('resize', this.getWindowWidth)
+  },
+  mounted () {
+    window.addEventListener('resize', this.getWindowWidth)
+    this.getWindowWidth()
+  }
+})
